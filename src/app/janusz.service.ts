baeb03e7ff32081b266aab16ce479089ae4e69b0
Emilian Lato <emilian3310@gmail.com>
2019-10-30 15:54:45 +0100

    Add messageService and component

--- src/app/janusz.service.ts
+++ src/app/janusz.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Janusz } from './janusz';
 import { JANUSZE } from './mock-janusze';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JanuszService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getJanusze(): Observable<Janusz[]> {
+    this.messageService.add('JanuszService: fetched janusze');
     return of(JANUSZE);
   }
 }
