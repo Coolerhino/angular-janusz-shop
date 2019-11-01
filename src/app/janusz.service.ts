258e1ce554ad0584731eb0dd8e184cdc1c497f7b
Emilian Lato <emilian3310@gmail.com>
2019-11-01 23:24:51 +0100

    Add fetching data from api
    Add http service
    Add error handling

--- src/app/janusz.service.ts
+++ src/app/janusz.service.ts
@@ -3,23 +3,52 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 import { Janusz } from './janusz';
-import { JANUSZE } from './mock-janusze';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class JanuszService {
 
-  constructor(private messageService: MessageService) { }
+  // TODO this need to be more configurable
+  private mockApiId = '';
+  private januszeUrl = `https://${this.mockApiId}.mockapi.io/janusze`;
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getJanusze(): Observable<Janusz[]> {
-    this.messageService.add('JanuszService: fetched janusze');
-    return of(JANUSZE);
+    return this.http.get<Janusz[]>(this.januszeUrl)
+      .pipe(
+        tap(_ => this.log('fetched janusze')),
+        catchError(this.handleError<Janusz[]>('getJanusze', []))
+      );
   }
 
   getJanusz(id: number): Observable<Janusz> {
-    this.messageService.add(`JanuszService: fetched janusz id=${id}`);
-    return of(JANUSZE.find(janusz => janusz.id === id));
+    const url = `${this.januszeUrl}/${id}`;
+    return this.http.get<Janusz>(url)
+      .pipe(
+        tap(_ => this.log(`fetched janusz id=${id}`)),
+        catchError(this.handleError<Janusz>('getJanusze'))
+      );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`JanuszService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed:${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
